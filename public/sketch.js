df615debc5f853140c466ec7878859ae2df9001c
SandyVu <sandyvu1311@gmail.com>
2023-05-09 18:54:59 +1000

    add comments

--- public/sketch.js
+++ public/sketch.js
@@ -34,28 +34,29 @@ function setup() {
 
 function draw() {
    
- 
+// The sound is played in loop when user clicks the mouse (anywhere within the canvas)
  if (mouseIsPressed){
     if ( river.isPlaying() ==false){
       river.loop()
     }
   } 
-  
+
+ // If the mouse is in the top-left corner of the canvas (both mouseX and mouseY are less than or equal to 100), the color of the stroke is set to red  
  if(mouseX <=100 & mouseY <=100){
     
     stroke('red')
-    
+  //If the mouse is in the bottom-right corner of the canvas (both mouseX and mouseY are greater than or equal to 300), the color of the stroke is set to blue  
   }else if (mouseX >=300 & mouseY >=300){
     
     stroke ('blue')    
-    
+  //If the mouse is to the right of the center of the canvas (mouseX is greater than 500), the color of the  stroke is set to yellow and the stroke weight is increased to 1.5   
   }else if(mouseX > 500){
     
     stroke ('yellow')
     strokeWeight(1.5)
-    
+  //If none of these conditions are met, the color of the particle stroke is set to cyan 
   }else { 
-     stroke ('purple')
+     stroke ('cyan')
   }
 
   for(let i = 0; i < num; i ++) {
