b912b8ef6392519b199dd73708ce2dddeb1238c0
SandyVu <sandyvu1311@gmail.com>
2023-05-12 11:51:42 +1000

    function

--- public/sketch.js
+++ public/sketch.js
@@ -42,23 +42,7 @@ function draw() {
     }
   } 
 
- // If the mouse is in the top-left corner of the canvas (both mouseX and mouseY are less than or equal to 100), the color of the stroke is set to red  
- if(mouseX <=100 & mouseY <=100){
-    
-    stroke('red')
-  //If the mouse is in the bottom-right corner of the canvas (both mouseX and mouseY are greater than or equal to 300), the color of the stroke is set to blue  
-  }else if (mouseX >=300 & mouseY >=300){
-    
-    stroke ('blue')    
-  //If the mouse is to the right of the center of the canvas (mouseX is greater than 500), the color of the  stroke is set to yellow and the stroke weight is increased to 1.5   
-  }else if(mouseX > 500){
-    
-    stroke ('yellow')
-    strokeWeight(1.5)
-  //If none of these conditions are met, the color of the particle stroke is set to cyan 
-  }else { 
-     stroke ('cyan')
-  }
+  change();
 
   for(let i = 0; i < num; i ++) {
     // create a new variable p and assign it the value of the i element in the particles array
@@ -90,6 +74,26 @@ function draw() {
 //It returns true if the point is within the canvas and false otherwise.
 function onScreen(v) {
   return v.x >= 0 && v.x <= width && v.y >= 0 && v.y <= height;
+}
+
+function change(){
+ // If the mouse is in the top-left corner of the canvas (both mouseX and mouseY are less than or equal to 100), the color of the stroke is set to red  
+ if(mouseX <=100 & mouseY <=100){
+    
+  stroke('red')
+//If the mouse is in the bottom-right corner of the canvas (both mouseX and mouseY are greater than or equal to 300), the color of the stroke is set to blue  
+}else if (mouseX >=300 & mouseY >=300){
+  
+  stroke ('blue')    
+//If the mouse is to the right of the center of the canvas (mouseX is greater than 500), the color of the  stroke is set to yellow and the stroke weight is increased to 1.5   
+}else if(mouseX > 500){
+  
+  stroke ('yellow')
+  strokeWeight(1.5)
+//If none of these conditions are met, the color of the particle stroke is set to cyan 
+}else { 
+   stroke ('cyan')
+}
 }
 
 
